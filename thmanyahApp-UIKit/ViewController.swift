d7d342bae10df9a9630049636893db3dcf398e32
vdotup <v.up@live.com>
2021-12-22 18:39:05 +0300

    Initial Commit

---
+++ thmanyahApp-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  thmanyahApp-UIKit
+//
+//  Created by Abdurrahman Alfudeghi on 22/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
