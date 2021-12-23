728847134651e51685950fc9da49b556d182c554
vdotup <v.up@live.com>
2021-12-23 14:04:02 +0300

    changed Color extension to UIColor

---
+++ thmanyahApp-UIKit/Extensions/UIColor+.swift
@@ -0,0 +1,13 @@
+//
+//  Color+.swift
+//  thmanyahApp-UIKit
+//
+//  Created by Abdurrahman Alfudeghi on 22/12/2021.
+//
+
+import UIKit
+
+extension UIColor {
+    static var vermilion: UIColor = .init(red: 219/255, green: 60/255, blue: 29/255, alpha: 1.0)
+    static var bone: UIColor      = .init(red: 225/255, green: 220/255, blue: 204/255, alpha: 1.0)
+}
